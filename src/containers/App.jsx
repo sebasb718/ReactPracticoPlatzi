8b33cb430ce9749fd2930f3a829b469ad1432ac4
Sebastian Bernal <sebasb718@gmail.com>
2021-03-12 17:06:36 -0500

    Clase 27, implementando custom hooks y propTypes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,44 +1,40 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initalState';
+
 const App = () => {
-  const [videos, setVideos] = useState([]);
-  useEffect(() => {
-    fetch('http://localhost:3000/initalState')
-      .then((response) => response.json())
-      .then((data) => setVideos(data));
-  }, []);
-  return (
+  const initialState = useInitialState(API);
+  return initialState.lenght === 0 ? <h1>Loading...</h1> : (
     <div className='App'>
       <Header />
       <Search />
       {
-        videos.mylist?.lenght > 0 && (
+        initialState.mylist?.lenght > 0 && (
           <Categories title='Mi lista'>
             <Carousel>
               <CarouselItem />
-
             </Carousel>
           </Categories>
         )
       }
-
       <Categories title='Tendencias'>
         <Carousel>
-          {videos.trends?.map((item) => <CarouselItem key={item.id} {...item} />)}
+          {initialState.trends?.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
 
       <Categories title='Originales de PLatziVideo'>
         <Carousel>
-          {videos.originals?.map((item) => <CarouselItem key={item.id} {...item} />)}
+          {initialState.originals?.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
       <Footer />
