fcdfd53e9d03c7a05e2b1773ec2f2d53193ac3dc
Sebastian Bernal <sebasb718@gmail.com>
2021-03-12 00:15:03 -0500

    Clase 21, terminada la creacion y estilizacion de componentes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -11,8 +11,25 @@ const App = () => (
   <div className='App'>
     <Header />
     <Search />
-    <Categories>
+    <Categories title='Mi lista'>
       <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title='Tendencias'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
