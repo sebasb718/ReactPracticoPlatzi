686bef35561357787e0bbff73e12457190ce4fba
Sebastian Bernal <sebasb718@gmail.com>
2021-03-12 16:45:47 -0500

    Clase 25, implementador react hooks y usando data de fake API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
@@ -11,39 +12,33 @@ const App = () => {
   const [videos, setVideos] = useState([]);
   useEffect(() => {
     fetch('http://localhost:3000/initalState')
-      .then(response => response.json())
-      .then(data => setVideos(data));
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
   }, []);
-
-  console.log(videos);
-
   return (
     <div className='App'>
       <Header />
       <Search />
-      <Categories title='Mi lista'>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {
+        videos.mylist?.lenght > 0 && (
+          <Categories title='Mi lista'>
+            <Carousel>
+              <CarouselItem />
+
+            </Carousel>
+          </Categories>
+        )
+      }
 
       <Categories title='Tendencias'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {videos.trends?.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+
+      <Categories title='Originales de PLatziVideo'>
+        <Carousel>
+          {videos.originals?.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
       <Footer />
